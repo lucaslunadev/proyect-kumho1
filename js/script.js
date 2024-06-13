4286eeab7848c58d031042b14b9d319dce5ccacd
lucaslunadev <lucasluna@outlook.com>
2024-06-13 17:53:48 -0300

    commit initial

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+/* SECTION-MARCAS
+ */
+
+var copy = document.querySelector(".logos-slide").cloneNode(true);
+document.querySelector(".logos").appendChild(copy);
+
+
+//deshabilitar zoom
+
+/* document.addEventListener('gesturestart', function (e) {
+    e.preventDefault();
+});
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
